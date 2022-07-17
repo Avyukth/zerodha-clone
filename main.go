0383605f65782cf01d4e58ed283419d32539a39f
Avyukth <makur.subhrajit@gmail.com>
2022-07-18 02:29:42 +0530

    docker file chnage

--- main.go
+++ main.go
@@ -2,13 +2,27 @@ package main
 
 import (
 	"fmt"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
+	"os"
 	"zerodha-clone/routers"
 )
 
 func main() {
+	var port string
+	if os.Getenv("ENV") == "PROD" {
+		port = os.Getenv("PROD_APP_PORT")
+
+	} else {
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatalf("Error loading .env file")
+		}
+		port = os.Getenv("DEV_APP_PORT")
+	}
+	portString := ":" + port
 	r := routers.Router()
-	fmt.Println("Starting server on port 8080..........")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	fmt.Printf("Starting server on port %s..........\n", port)
+	log.Fatal(http.ListenAndServe(portString, r))
 }
